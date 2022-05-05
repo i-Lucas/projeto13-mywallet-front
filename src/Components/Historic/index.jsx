5d6e3d668fc3dd6bc2abc11cab26311a844f0333
i-Lucas <novo.contato.lucas@gmail.com>
2022-05-05 03:01:43 -0300

    update historic screen

--- src/Components/Historic/index.jsx
+++ src/Components/Historic/index.jsx
@@ -1,3 +1,42 @@
+import HistoricContainer from "./style"
+
 export default function Historic() {
-    return <h1>Historic</h1>
+
+    return (
+
+        <HistoricContainer>
+
+            <div className="username-exit">
+                <h1>Hi Lucas</h1>
+                <div className="icon"><ion-icon name="log-out-outline"></ion-icon></div>
+            </div>
+
+            <div className="records">
+                <div className="screen">
+                    <h1>teste</h1>
+                </div>
+
+                <div className="options">
+                    <div className="new-entry">
+                        <div className="icon">
+                            <ion-icon name="add-circle-outline"></ion-icon>
+                        </div>
+                        <div className="text">
+                            <h1>new entry</h1>
+                        </div>
+                    </div>
+                    <div className="new-output">
+                        <div className="icon">
+                            <ion-icon name="remove-circle-outline"></ion-icon>
+                        </div>
+                        <div className="text">
+                            <h1>new outlay</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </HistoricContainer>
+
+    )
 }
---
+++ src/Components/Historic/style.jsx
@@ -0,0 +1,179 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+
+    width: 100%;
+    height: 100vh;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #8C11BE;
+
+    @media (min-width: 768px) {
+
+        width: 50%;
+        margin-left: 25%;
+    }
+
+    .username-exit {
+
+        width: 100%;
+        height: 10%;
+
+        display: flex;
+        justify-content: space-between;
+        align-items: flex-end;
+
+        padding-left: 6%;
+        padding-right: 6%;
+
+        font-family: 'Raleway';
+        font-weight: 700;
+        font-size: 26px;
+        line-height: 31px;
+        color: #FFFFFF;
+
+        .icon {
+            
+            cursor: pointer;
+            ion-icon {
+
+                font-size: 35px;
+    
+              }
+        }
+    }
+
+    .records {
+
+        width: 90%;
+        height: 85%;
+        background-color: #8C11BE;
+
+        .screen { 
+
+            width: 100%;
+            height: 80%;
+            background-color: #FFFFFF;
+            border-radius: 5px;
+
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .options {
+
+            width: 100%;
+            height: 20%;
+
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            background-color: #8C11BE;
+
+            .new-entry {
+    
+                width: 45%;
+                height: 85%;
+                background: #A328D6;
+                border-radius: 5px;
+
+                .icon {
+
+                    width: 25%;
+                    height: 50%;
+
+                    display: flex;
+                    justify-content: center;
+                    flex-direction: column;
+                    align-items: flex-end;
+                    cursor: pointer;
+
+                    ion-icon {
+
+                        font-size: 35px;
+                        color: #FFFFFF;
+
+                      }
+                }
+
+                .text {
+
+                    width: 100%;
+                    height: 50%;
+
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+
+                    h1 {
+                        
+                        font-family: 'Raleway';
+                        font-weight: 700;
+                        font-size: 17px;
+                        line-height: 20px;
+                        color: #FFFFFF;
+                    }
+                }
+
+
+            }
+    
+            .new-output {
+    
+                width: 45%;
+                height: 85%;
+                background: #A328D6;
+                border-radius: 5px;    
+
+                .icon {
+
+                    width: 25%;
+                    height: 50%;
+
+                    display: flex;
+                    justify-content: center;
+                    flex-direction: column;
+                    align-items: flex-end;
+                    cursor: pointer;
+
+                    cursor: pointer;
+
+                    ion-icon {
+
+                        font-size: 35px;
+                        color: #FFFFFF;
+                        
+                      }
+                }
+
+                .text {
+
+                    width: 100%;
+                    height: 50%;
+
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+
+                    h1 {
+                        
+                        font-family: 'Raleway';
+                        font-weight: 700;
+                        font-size: 17px;
+                        line-height: 20px;
+                        color: #FFFFFF;
+                    }
+                }            
+
+                
+            }
+        }
+    }  
+`
+
+
+export default Container;
